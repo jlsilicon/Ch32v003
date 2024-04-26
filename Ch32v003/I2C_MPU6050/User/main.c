9d477898d288e08d31670af637db9984772908ef
Long John Silicon <52719931+jlsilicon@users.noreply.github.com>
2024-04-26 08:04:10 -0400

    Update main.c

--- Ch32v003/I2C_MPU6050/User/main.c
+++ Ch32v003/I2C_MPU6050/User/main.c
@@ -4,6 +4,8 @@ The VCC and GND pins of the CH32V103 development board are connected to the VCC 
  PA1 = SDA : The PA1 pin of the CH32V103 development board is connected to the SDA pin of the MPU6050 module
  PA2 = SCL : The PA2 pin of the CH32V103 development board is connected to the SCL pin of the MPU6050 module
 
+Mpu6050 : Id Address = 0x58 wrt , 0x59 rd
+
  - Compiles Downloads , Returns same data : Temp=175, AX=-11823 , AY=-11823 , AZ=-11823 , GX=-11823 , AGY=-11823 , GZ=-11823
 
  - TX = PA9
@@ -11,81 +13,26 @@ The VCC and GND pins of the CH32V103 development board are connected to the VCC 
 
 */
 
-///
-
-// iic.h : //
-
-#ifndef __I2C_H
-#define __I2C_H
-
-// #include "ch32v10x.h"
-#include "ch32v00x.h"
-
-void I2C1_Init(void);
-
-#endif  /* __I2C_H */
-
-///
-
-// iic.c : //
-
-// #include "i2c.h"
-
-/*****************************************************
- * @brief   I2C1 SCL SDA
- * @note    PB8 IIC1 SCL PB9 IIC1 SDA
- * @param
- * @retval
-*****************************************************/
-
-static void I2C1_GPIO_Config(void)
-{
 /*
-    GPIO_InitTypeDef GPIO_InitStruce;   //IIC1 GPIO
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO,ENABLE);  //GPIOB、RCC
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1,ENABLE);                         //I2C1 RCC
+ *@Note
+ I2C interface routine to operate EEPROM peripheral:
+ I2C1_SCL(PC2)\I2C1_SDA(PC1).
+  This example uses EEPROM for AT24Cxx series.
+  Steps:
+ READ EEPROM:Start + 0xA0 + 8bit Data Address + Start + 0xA1 + Read Data + Stop.
+ WRITE EERPOM:Start + 0xA0 + 8bit Data Address + Write Data + Stop.
 
-    GPIO_PinRemapConfig(GPIO_Remap_I2C1, ENABLE);   //I2C1
-
-    GPIO_InitStruce.GPIO_Mode = GPIO_Mode_AF_OD;    //
-    GPIO_InitStruce.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_InitStruce.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-    GPIO_Init(GPIOB, &GPIO_InitStruce);
 */
-}
+
+#include "debug.h"
+
+///
 
 
-/*****************************************************
- * @brief   I2C1
- * @note    SCL 400KHz,IIC 2:1,0x52,
- * @param
- * @retval
-*****************************************************/
-static void I2C1_Mode_Config(void)
-{
-    I2C_InitTypeDef I2C_InitStruce;                 //定义IIC1
-
-    I2C_InitStruce.I2C_Mode                = I2C_Mode_I2C;         //I2C
-    I2C_InitStruce.I2C_ClockSpeed          = 400000;         //SCL 400KHz
-    I2C_InitStruce.I2C_OwnAddress1         = 0x52;          //0x52
-    I2C_InitStruce.I2C_Ack                 = I2C_Ack_Enable;        //
-    I2C_InitStruce.I2C_DutyCycle           = I2C_DutyCycle_2; //2:1
-    I2C_InitStruce.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;  //7
-    I2C_Init(I2C1, &I2C_InitStruce);                //I2C1
-}
-
-/*****************************************************
- * @brief   I2C1
- * @note
- * @param
- * @retval
-*****************************************************/
-void I2C1_Init(void)
-{
-    I2C1_GPIO_Config();     //I2C1
-    I2C1_Mode_Config();     //I2C1
-    I2C_Cmd(I2C1, ENABLE);  //I2C1
-}
+u8   AT24CXX_ReadOneByte(  u16 ReadAddr ) ;
+void AT24CXX_WriteOneByte( u16 WriteAddr, u8 DataToWrite ) ;
+void AT24CXX_Read(         u16 ReadAddr, u8 *pBuffer, u16 NumToRead ) ;
+void AT24CXX_Write(        u16 WriteAddr, u8 *pBuffer, u16 NumToWrite ) ;
 
 
 ///
@@ -114,8 +61,9 @@ void I2C1_Init(void)
 */
 
 
-//#define MPU_ACCEL_OFFS_REG        0X06    //accel_offs
-//#define MPU_PROD_ID_REG           0X0C    //prod id
+ #define MPU_ACCEL_OFFS_REG        0X06    //accel_offs
+ #define MPU_PROD_ID_REG           0X0C    //prod id
+
 #define MPU_SELF_TESTX_REG      0X0D    //X
 #define MPU_SELF_TESTY_REG      0X0E    //Y
 #define MPU_SELF_TESTZ_REG      0X0F    //Z
@@ -211,9 +159,12 @@ u8 MPU_Get_Accelerometer(short *ax,short *ay,short *az);
 
 ///
 
+// MPU6050.c //
 
 // #include "mpu6050.h"
+/*
 #include "debug.h"
+*/
 
 //
 
@@ -293,8 +244,8 @@ u8 MPU_Init(void)
     MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X80); //MPU6050
     Delay_Ms(100);
     MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X00); //MPU6050
-    MPU_Set_Gyro_Fsr(3);                    //±2000dps
-    MPU_Set_Accel_Fsr(0);                   //±2g
+    MPU_Set_Gyro_Fsr(3);                    //隆脌2000dps
+    MPU_Set_Accel_Fsr(0);                   //隆脌2g
     MPU_Set_Rate(50);                       //50Hz
     MPU_Write_Byte(MPU_INT_EN_REG,0X00);    //
     MPU_Write_Byte(MPU_USER_CTRL_REG,0X00); //I2C
@@ -314,6 +265,8 @@ u8 MPU_Init(void)
 }
 
 */
+
+/*
 
 void  IIC_Init(u32 bound, u16 address)
 // u8  MPU_Init(void)
@@ -381,30 +334,49 @@ int  res ;
 
 }
 
+*/
 
-//fsr:0,±250dps;1,±500dps;2,±1000dps;3,±2000dps
+
+//fsr:0,250dps;1,500dps;2,1000dps;3,2000dps
 u8 MPU_Set_Gyro_Fsr(u8 fsr)
 {
-    return MPU_Write_Byte(MPU_GYRO_CFG_REG,fsr<<3);//
+// //    return( MPU_Write_Byte(       MPU_GYRO_CFG_REG , fsr << 3 ) );//
+            AT24CXX_WriteOneByte( MPU_GYRO_CFG_REG , fsr << 3 )  ;
+
+return( 0 );
 }
 
 u8 MPU_Set_Accel_Fsr(u8 fsr)
 {
-    return MPU_Write_Byte(MPU_ACCEL_CFG_REG,fsr<<3);//
+// // return MPU_Write_Byte(       MPU_ACCEL_CFG_REG , fsr << 3 );//
+       AT24CXX_WriteOneByte( MPU_ACCEL_CFG_REG , fsr << 3 )  ;
+
+return( 0 );
 }
+
 
 u8 MPU_Set_LPF(u16 lpf)
 {
-u8 data=0;
+u8 data = 0;
 
-    if(lpf>=188)data=1;
-    else if(lpf>=98)data=2;
-    else if(lpf>=42)data=3;
-    else if(lpf>=20)data=4;
-    else if(lpf>=10)data=5;
-    else data=6;
+    if(      lpf >= 188 )
+        data=1;
+    else if( lpf >= 98 )
+        data=2;
+    else if( lpf >= 42 )
+        data=3;
+    else if( lpf >= 20 )
+        data=4;
+    else if( lpf >= 10 )
+        data=5;
+    else
+        data = 6;
 
-return MPU_Write_Byte(MPU_CFG_REG,data);//
+// // return( MPU_Write_Byte(       MPU_CFG_REG , data ) );
+// return( AT24CXX_WriteOneByte( MPU_CFG_REG , data ) );
+    AT24CXX_WriteOneByte( MPU_CFG_REG , data )  ;
+
+return(0);
 }
 
 
@@ -412,14 +384,16 @@ u8 MPU_Set_Rate(u16 rate)
 {
 u8 data;
 
-    if(rate>1000)
-        rate=1000;
-    if(rate<4)
-        rate=4;
-    data=1000/rate-1;
-    data=MPU_Write_Byte(MPU_SAMPLE_RATE_REG,data);  //
+    if(rate > 1000)
+        rate = 1000;
+    if(rate < 4)
+        rate = 4;
+    data = 1000 / rate - 1 ;
 
-    return MPU_Set_LPF(rate/2); //LPF
+// //    data = MPU_Write_Byte(       MPU_SAMPLE_RATE_REG , data );  //
+           AT24CXX_WriteOneByte( MPU_SAMPLE_RATE_REG , data );    //
+
+return MPU_Set_LPF( rate / 2 ); //LPF
 }
 
 
@@ -429,35 +403,43 @@ u8 buf[2];
 short raw;
 float temp;
 
-    MPU_Read_Len(MPU_ADDR,MPU_TEMP_OUTH_REG,2,buf);
-    raw = ((u16)buf[0]<<8) | buf[1];
+// //    MPU_Read_Len( MPU_ADDR , MPU_TEMP_OUTH_REG , 2 , buf );
+    AT24CXX_Read(            MPU_TEMP_OUTH_REG , buf , 2 );
+
+    raw  = ((u16)buf[0]<<8) | buf[1];
     temp = 36.53 + ((double)raw) / 340;
 
-return temp*100;;
+return( temp * 100 );
 }
 
 
 u8 MPU_Get_Gyroscope(short *gx,short *gy,short *gz)
 {
-    u8 buf[6],res;
-    res=MPU_Read_Len(MPU_ADDR,MPU_GYRO_XOUTH_REG,6,buf);
-    if(res==0)
+u8 buf[6],res = 0 ;
+
+// //    res = MPU_Read_Len( MPU_ADDR , MPU_GYRO_XOUTH_REG , 6 , buf );
+         AT24CXX_Read(            MPU_GYRO_XOUTH_REG , buf , 6 );
+
+// //    if( res == 0 )
     {
         *gx = ((u16)buf[0]<<8) | buf[1];
         *gy = ((u16)buf[2]<<8) | buf[3];
         *gz = ((u16)buf[4]<<8) | buf[5];
         printf("GYRO : X=%d , Y=%d , Z=%d  \r\n",*gx,*gy,*gz);
     }
-    return res;;
+
+return res;;
 }
 
 
 u8 MPU_Get_Accelerometer(short *ax,short *ay,short *az)
 {
-u8 buf[6],res;
+u8 buf[6] , res = 0 ;
 
-    res=MPU_Read_Len(MPU_ADDR,MPU_ACCEL_XOUTH_REG,6,buf);
-    if(res==0)
+// //    res = MPU_Read_Len( MPU_ADDR , MPU_ACCEL_XOUTH_REG , 6 , buf );
+          AT24CXX_Read(            MPU_ACCEL_XOUTH_REG , buf , 6 );
+
+// //    if( res == 0 )
     {
         *ax = ((u16)buf[0]<<8) | buf[1];
         *ay = ((u16)buf[2]<<8) | buf[3];
@@ -529,22 +511,23 @@ printf( " <%02X> " , * buf );
 
 u8 MPU_Write_Byte(u8 reg,u8 data)
 {
-    IIC_Start();
+  IIC_Start();
     IIC_SendByte((MPU_ADDR<<1)|0);//
     if(IIC_WaitAck())  //
     {
         IIC_Stop();
-        return 1;
+return 1;
     }
     IIC_SendByte(reg); //
     IIC_WaitAck();     //
+
     IIC_SendByte(data);//
     if(IIC_WaitAck())  //ACK
     {
         IIC_Stop();
-        return 1;
+return 1;
     }
-    IIC_Stop();
+  IIC_Stop();
 
 return 0;
 }
@@ -573,11 +556,15 @@ return res;
 }
 
 
-int main(void)
+// int main(void)
+// int main_1(void)
+int  MPU6050_TEST(void)
 {
 short aacx,aacy,aacz;       //
 short gyrox,gyroy,gyroz;    //
 short temp;                 //
+
+/*
 
         NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
         Delay_Init();
@@ -590,6 +577,8 @@ short temp;                 //
         printf("\r\n\r\n: MPU6050 TEST : \r\n\r\n");
         printf("- SystemClk : %d \r\n\r\n\r\n",SystemCoreClock);
 
+*/
+
         while( 1 )
         {
             temp = MPU_Get_Temperature(); //
@@ -601,6 +590,337 @@ short temp;                 //
              Delay_Ms(500);
 
         }
+}
+
+///
+
+
+/**********************************************************************
+*@Note:
+AT24Cxx
+
+READ EEPROM Start + 0xA0 + 8bit Data Address + Start + 0xA1 + Read Data + Stop.
+WRITE EERPOM Start + 0xA0 + 8bit Data Address + Write Data + Stop.
+*******************************************************************************/
+/* EERPOM DATA ADDRESS Length Definition */
+#define Address_8bit  0
+#define Address_16bit  1
+
+/* EERPOM DATA ADDRESS Length Selection */
+#define Address_Lenth   Address_8bit
+//#define Address_Lenth   Address_16bit
+
+/* Global define */
+#define SIZE sizeof(TEXT_Buffer)
+
+/* Global Variable */
+const u8 TEXT_Buffer[]={"CH32V00x I2C TEST"};
+
+/*********************************************************************
+ * @fn      IIC_Init
+ *
+ * @brief   Initializes the IIC peripheral.
+ *
+ * @return  none
+ */
+void IIC_Init(u32 bound, u16 address)
+{
+printf("> IIC_Init() :\r\n");
+
+    GPIO_InitTypeDef GPIO_InitStructure={0};
+    I2C_InitTypeDef I2C_InitTSturcture={0};
+
+    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE );
+    RCC_APB1PeriphClockCmd( RCC_APB1Periph_I2C1, ENABLE );
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init( GPIOC, &GPIO_InitStructure );
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init( GPIOC, &GPIO_InitStructure );
+
+    I2C_InitTSturcture.I2C_ClockSpeed = bound;
+    I2C_InitTSturcture.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitTSturcture.I2C_DutyCycle = I2C_DutyCycle_2;
+    I2C_InitTSturcture.I2C_OwnAddress1 = address;
+    I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_Init( I2C1, &I2C_InitTSturcture );
+
+    I2C_Cmd( I2C1, ENABLE );
+
+    I2C_AcknowledgeConfig( I2C1, ENABLE );
+
+printf("< IIC_Init() .\r\n");
+}
+
+/*********************************************************************
+ * @fn      AT24CXX_Init
+ *
+ * @brief   Initializes AT24xx EEPROM.
+ *
+ * @return  none
+ */
+void AT24CXX_Init(void)
+{
+    IIC_Init( 100000, 0xA0 );
+}
+
+void MPU6050_Init(void)
+{
+    IIC_Init( 100000, 0x68 );
+}
+
+
+/*********************************************************************
+ * @fn      AT24CXX_ReadOneByte
+ *
+ * @brief   Read one data from EEPROM.
+ *
+ * @param   ReadAddr - Read first address.
+ *
+ * @return  temp - Read data.
+ */
+u8 AT24CXX_ReadOneByte(u16 ReadAddr)
+{
+    u8 temp=0;
+
+    while( I2C_GetFlagStatus( I2C1, I2C_FLAG_BUSY ) != RESET );
+    I2C_GenerateSTART( I2C1, ENABLE );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
+// //    I2C_Send7bitAddress( I2C1, 0XA0, I2C_Direction_Transmitter );
+    I2C_Send7bitAddress( I2C1, 0X58, I2C_Direction_Transmitter );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
+
+/*
+    // Send Address 8b / 16b : //
+
+#if (Address_Lenth  == Address_8bit)
+    I2C_SendData( I2C1, (u8)(ReadAddr&0x00FF) );
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+
+#elif (Address_Lenth  == Address_16bit)
+    I2C_SendData( I2C1, (u8)(ReadAddr>>8) );
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+
+    I2C_SendData( I2C1, (u8)(ReadAddr&0x00FF) );
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+
+#endif
+*/
+
+    I2C_GenerateSTART( I2C1, ENABLE );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
+// //    I2C_Send7bitAddress( I2C1, 0XA0, I2C_Direction_Receiver );
+    I2C_Send7bitAddress( I2C1, 0X58, I2C_Direction_Receiver );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED ) );
+  while( I2C_GetFlagStatus( I2C1, I2C_FLAG_RXNE ) ==  RESET )
+    I2C_AcknowledgeConfig( I2C1, DISABLE );
+
+    temp = I2C_ReceiveData( I2C1 );
+  I2C_GenerateSTOP( I2C1, ENABLE );
+
+    return temp;
+}
+
+
+/*********************************************************************
+ * @fn      AT24CXX_WriteOneByte
+ *
+ * @brief   Write one data to EEPROM.
+ *
+ * @param   WriteAddr - Write frist address.
+ *
+ * @return  DataToWrite - Write data.
+ */
+void AT24CXX_WriteOneByte(u16 WriteAddr, u8 DataToWrite)
+{
+    while( I2C_GetFlagStatus( I2C1, I2C_FLAG_BUSY ) != RESET );
+    I2C_GenerateSTART( I2C1, ENABLE );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
+// //    I2C_Send7bitAddress( I2C1, 0XA0, I2C_Direction_Transmitter );
+    I2C_Send7bitAddress( I2C1, 0X58, I2C_Direction_Transmitter );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
+
+/*
+    // Send Address 8b / 16b : //
+
+#if (Address_Lenth  == Address_8bit)
+    I2C_SendData( I2C1, (u8)(WriteAddr&0x00FF) );
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+
+#elif (Address_Lenth  == Address_16bit)
+    I2C_SendData( I2C1, (u8)(WriteAddr>>8) );
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+
+    I2C_SendData( I2C1, (u8)(WriteAddr&0x00FF) );
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+
+#endif
+*/
+
+    if( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE ) !=  RESET )
+    {
+        I2C_SendData( I2C1, DataToWrite );
+    }
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+    I2C_GenerateSTOP( I2C1, ENABLE );
+}
+
+
+/*********************************************************************
+ * @fn      AT24CXX_Read
+ *
+ * @brief   Read multiple data from EEPROM.
+ *
+ * @param   ReadAddr - Read frist address. (AT24c02: 0~255)
+ *          pBuffer - Read data.
+ *          NumToRead - Data number.
+ *
+ * @return  none
+ */
+void AT24CXX_Read(u16 ReadAddr, u8 *pBuffer, u16 NumToRead)
+{
+    while( NumToRead )
+    {
+        * pBuffer ++ = AT24CXX_ReadOneByte( ReadAddr ++ );
+         NumToRead -- ;
+    }
+}
+
+
+/*********************************************************************
+ * @fn      AT24CXX_Write
+ *
+ * @brief   Write multiple data to EEPROM.
+ *
+ * @param   WriteAddr - Write frist address. (AT24c02: 0~255)
+ *          pBuffer - Write data.
+ *          NumToWrite - Data number.
+ *
+ * @return  none
+ */
+void AT24CXX_Write(u16 WriteAddr, u8 *pBuffer, u16 NumToWrite)
+{
+    while(NumToWrite--)
+    {
+        AT24CXX_WriteOneByte( WriteAddr , * pBuffer );
+         WriteAddr ++ ;
+         pBuffer ++ ;
+          Delay_Ms(2);
+    }
+}
+
+
+// // void MPU6050_Write(u16 WriteAddr, u8 *pBuffer, u16 NumToWrite)
+void MPU6050_Write()
+{
+printf("> MPU6050_Write() : \r\n");
+
+// //     MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X80); //MPU6050
+     AT24CXX_WriteOneByte(MPU_PWR_MGMT1_REG,0X80); //MPU6050
+      Delay_Ms(100);
+// //     MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X00); //MPU6050
+     AT24CXX_WriteOneByte(MPU_PWR_MGMT1_REG,0X00); //MPU6050
+//      Delay_Ms(1);
+
+printf("= MPU6050_Write() -> MPU_Set_Gyro_Fsr() \r\n");
+// //     MPU_Set_Gyro_Fsr(3);                    //+-2000dps
+     AT24CXX_WriteOneByte( MPU_ACCEL_CFG_REG , 3 << 3 )  ;
+//      Delay_Ms(1);
+printf("= MPU6050_Write() -> MPU_Set_Accel_Fsr() \r\n");
+     MPU_Set_Accel_Fsr(0);                   //+-2g
+printf("= MPU6050_Write() -> MPU_Set_Rate() \r\n");
+     MPU_Set_Rate(50);                       //50Hz
+
+printf("= MPU6050_Write() ... \r\n");
+
+// //     MPU_Write_Byte(MPU_INT_EN_REG,0X00);    //
+     AT24CXX_WriteOneByte(MPU_INT_EN_REG,0X00);    //
+// //     MPU_Write_Byte(MPU_USER_CTRL_REG,0X00); //I2C
+     AT24CXX_WriteOneByte(MPU_USER_CTRL_REG,0X00); //I2C
+// //     MPU_Write_Byte(MPU_FIFO_EN_REG,0X00);   //FIFO
+     AT24CXX_WriteOneByte(MPU_FIFO_EN_REG,0X00);   //FIFO
+// //     MPU_Write_Byte(MPU_INTBP_CFG_REG,0X80); //INT
+     AT24CXX_WriteOneByte(MPU_INTBP_CFG_REG,0X80); //INT
+
+// // int  res ;
+// //     res = MPU_Read_Byte(MPU_DEVICE_ID_REG);
+     AT24CXX_ReadOneByte( MPU_DEVICE_ID_REG );
+// //     if( res == MPU_ADDR )  // ID
+     {
+// //         MPU_Write_Byte(MPU_PWR_MGMT1_REG,0X01); // CLKSEL,PLL X
+         AT24CXX_WriteOneByte(MPU_PWR_MGMT1_REG,0X01); // CLKSEL,PLL X
+// //         MPU_Write_Byte(MPU_PWR_MGMT2_REG,0X00); //
+         AT24CXX_WriteOneByte(MPU_PWR_MGMT2_REG,0X00); //
+
+         MPU_Set_Rate(50);                       // 50Hz
+     }
+ //    else
+ //      return 1;
+ //    return 0;
+
+printf("< MPU6050_Write() . \r\n");
+}
+
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
+// u8 data[SIZE];
+
+    Delay_Init();
+    USART_Printf_Init(115200);
+// //    printf(": eeprom 24Cxx :\r\n");
+    printf("\r\n\r\n: MPU6050 003 :\r\n");
+     printf("- SystemClk:%d\r\n",SystemCoreClock);
+
+// //    AT24CXX_Init();
+    MPU6050_Init();
+
+/*
+    printf("Start Write 24Cxx....\r\n");
+     AT24CXX_Write(100,(u8*)TEXT_Buffer,SIZE);
+    printf("24Cxx Write Success!\r\n");
+
+    Delay_Ms(500);
+
+    printf("Start Read 24Cxx....\r\n");
+     AT24CXX_Read(100,data,SIZE);
+    printf("The Data Read Is: \r\n");
+     printf("%s\r\n", data);
+*/
+
+    MPU6050_Write();
+
+    while( 1 )
+    {
+
+        printf("> mpu6050 Test ... \r\n");
+
+        MPU6050_TEST();
+
+         Delay_Ms(500);
+
+    }
+
 }
 
 ///
